e181370fa8622702e4b73956b2d57e1ad1c8f53d
Olivier Dembour <33929198+alex-sector@users.noreply.github.com>
2021-01-26 14:15:32 +0100

    Fix DNS

--- base/scripts/update-dns.ps1
+++ base/scripts/update-dns.ps1
@@ -4,9 +4,9 @@ Param (
 
 $subnet = $Ip -replace "\.\d+$", ""
 
-$newDNSServers = "127.0.0.1", $dns
+$newDNSServers = "127.0.0.1", $Ip
 $adapters = Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object { $_.IPAddress -And ($_.IPAddress).StartsWith($subnet) }
 if ($adapters) {
-    Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Setting DNS to $dns"
+    Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Setting DNS to $newDNSServers"
     $adapters | ForEach-Object {$_.SetDNSServerSearchOrder($newDNSServers)}
 }
